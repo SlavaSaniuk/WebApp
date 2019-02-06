d76656d8c296f47748e432870b6f52d7bccbf95b
SlavaSaniuk <saniuk.vyacheslav.97@gmail.com>
2019-02-07 00:51:23 +0300

    Root application context configuration class.

--- src/main/java/by/bsac/main/RootAppContext.java
+++ src/main/java/by/bsac/main/RootAppContext.java
@@ -1,19 +1,17 @@
 package by.bsac.main;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 /**
- *
+ *  Root application context configuration class.
+ *  Parent context for all other context.
+ *  Class contains all beans definition.
  */
 @Configuration
+// Import configuration class for data manipulation.
+@Import(by.bsac.data.DataConfiguration.class)
 public class RootAppContext {
 
-    /** Class global variables */
-
-
-    /** Class constructors */
-
-
-    /** Class methods */
 
 }
