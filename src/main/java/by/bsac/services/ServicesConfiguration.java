966197fef09067a33626d153591b1131e84c57ad
SlavaSaniuk <saniuk.vyacheslav.97@gmail.com>
2019-02-13 00:42:42 +0300

    Configuration class for all services beans.

--- src/main/java/by/bsac/services/ServicesConfiguration.java
+++ src/main/java/by/bsac/services/ServicesConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Description;
+import org.springframework.context.annotation.Scope;
 
 /**
  * Configuration class to define and handle application services.
@@ -13,12 +14,21 @@ import org.springframework.context.annotation.Description;
 @Configuration
 public class ServicesConfiguration {
 
+    /**
+     * Service used for authenticate / register users in system.
+     * Service has two methods: authenticate user in system and register them.
+     * @param user_dao - User DAO implementation from DataConfiguration class.
+     * @return - Authentication service bean.
+     */
     @Bean("authentication_service")
     @Description("Authentication service user for authenticate/register user in system. ")
-    @Autowired
+    @Autowired // UserDAO bean from DataConfiguration class.
+    @Scope("request")
     public AuthenticationService getAuthenticationService(UserDao user_dao) {
 
+        //Return statement
         return new AuthenticationService(user_dao);
+
     }
 
 
