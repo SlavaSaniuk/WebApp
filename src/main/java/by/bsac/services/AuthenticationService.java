23821135b02ae6023be265451956cfb49ad6c757
SlavaSaniuk <saniuk.vyacheslav.97@gmail.com>
2019-02-13 00:41:06 +0300

    Authenticate / create users in system.

--- src/main/java/by/bsac/services/AuthenticationService.java
+++ src/main/java/by/bsac/services/AuthenticationService.java
@@ -2,19 +2,22 @@ package by.bsac.services;
 
 import by.bsac.data.dao.UserDao;
 import by.bsac.models.User;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
- *  Authentication service used for authenticate users in system, or create users in system.
- *  Work with
+ *  Authentication service used for authenticate users in system, or create users rows in database.
+ *  Work with user DAO implementation.
  */
 @Service("authentication_service")
 public class AuthenticationService {
 
+    // user DAO implementation.
     private UserDao user_dao;
 
-    @Autowired
+    /**
+     * Create new authentication service with specified user DAO implementation.
+     * @param a_user_dao - custom user DAO implementation.
+     */
     public AuthenticationService(UserDao a_user_dao ) {
 
         //Mapping users DAO
@@ -22,8 +25,14 @@ public class AuthenticationService {
 
     }
 
+    /**
+     * Register specified user object in system. Create new user row in database.
+     * @param a_user - custom user object. Must contain a user email a user password values.
+     *                  User email value must be unique.
+     */
     public void registerUser(User a_user) {
 
+        //Create user row in database.
         this.user_dao.create(a_user);
 
     }
