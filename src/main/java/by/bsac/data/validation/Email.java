73b6aec2b37e426d737460c1b9a99d8cf2e69749
SlavaSaniuk <saniuk.vyacheslav.97@gmail.com>
2019-02-21 00:51:27 +0300

    @Email annotation

--- src/main/java/by/bsac/data/validation/Email.java
+++ src/main/java/by/bsac/data/validation/Email.java
@@ -1,6 +1,7 @@
 package by.bsac.data.validation;
 
 import javax.validation.Constraint;
+import javax.validation.Payload;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -12,6 +13,7 @@ import java.lang.annotation.Target;
 @Constraint(validatedBy = EmailConstraintValidator.class)
 public @interface Email {
 
-    public String message() default "email does not match global email pattern.";
-
+    String message() default "email does not match global email pattern.";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
 }
