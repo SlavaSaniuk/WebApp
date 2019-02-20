661eadadc26791276bf3c4a48c2ca12bc6774a94
SlavaSaniuk <saniuk.vyacheslav.97@gmail.com>
2019-02-21 00:52:17 +0300

    Email validation logic class

--- src/main/java/by/bsac/data/validation/EmailConstraintValidator.java
+++ src/main/java/by/bsac/data/validation/EmailConstraintValidator.java
@@ -2,14 +2,18 @@ package by.bsac.data.validation;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
+import java.util.regex.Pattern;
 
 /**
  *
  */
 public class EmailConstraintValidator implements ConstraintValidator<Email, String> {
 
+    private final Pattern EMAIL_SYNTAX_PATTERN = Pattern.compile("(\\w+[\\.\\-#&\\[\\]\\{\\}\\*\\+\\?\\(\\)]?\\w+)+" +
+                    "@(\\w+[\\.\\-#&\\[\\]\\{\\}\\*\\+\\?\\(\\)]?\\w+)+" +
+                    "\\.[a-z]{2,4}", Pattern.CASE_INSENSITIVE);
     @Override
     public boolean isValid(String value, ConstraintValidatorContext context) {
-        return false;
+        return Pattern.matches(EMAIL_SYNTAX_PATTERN.pattern(), value);
     }
 }
